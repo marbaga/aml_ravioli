02fbc882b4b2b06b0293459b4250977963afc3f8
Lorenzo Laneve <lore97drk@icloud.com>
2019-10-10 10:30:49 +0200

    Add outlier detection modules

--- aml1_pipeline.py
+++ aml1_pipeline.py
@@ -3,17 +3,21 @@ import pandas as pd
 import numpy as np
 
 from sklearn.impute import SimpleImputer
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.svm import OneClassSVM
 from sklearn.pipeline import Pipeline
 
 X_t = pd.read_csv('task1/X_train.csv', ',')
 y_t = pd.read_csv('task1/y_train.csv', ',')
 
 pipe = Pipeline([
-                 ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean'))
+                 ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
+                 ('outlier_detection', LocalOutlierFactor())
                  ])
 
 pipe = pipe.fit(X_t, y_t)
 
-X_t_filled = pipe.named_steps['imputer'].transform(X_t)
+X_t_filled = pipe.transform(X_t)
 
 pd.DataFrame(X_t_filled[0:, 1:]).to_csv('task1/output.csv', ',')
