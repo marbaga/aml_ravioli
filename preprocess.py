09b39c54ddf9bf08d70c500976ddb78606ec5e26
Lorenzo Laneve <lore97drk@icloud.com>
2019-10-12 11:31:32 +0200

    Change mean with median and add local outlier factor in outlier detection

--- preprocess.py
+++ preprocess.py
@@ -17,23 +17,41 @@ X_t = pd.read_csv('task1/X_train.csv', ',')
 y_t = pd.read_csv('task1/y_train.csv', ',')
 
 
-filler = SimpleImputer(missing_values=np.nan, strategy='mean')
+filler = SimpleImputer(missing_values=np.nan, strategy='median')
 filler.fit(X_t, y_t)
 
 X_t = filler.transform(X_t)
 pd.DataFrame(X_t).to_csv('task1/results/filled.csv', ',', index=False)
 
-outlier_detection = IsolationForest(behaviour='new')
-outlier_detection.fit(X_t[0:, 1:], y_t)
 
-outlier = outlier_detection.predict(X_t[0:, 1:])
-pd.DataFrame(outlier).to_csv('task1/results/outlier-decisions.csv', ',', index=False)
+outlier_detections = [
+    ('if', 'Isolation Forest', IsolationForest(behaviour='new', contamination='auto')),
+    ('loc', 'Local Outlier Factor', LocalOutlierFactor(contamination='auto'))
+]
 
-X_t = filter(lambda x: (outlier[int(x[0])] > 0), X_t)
+decisions = {}
+
+for (id, name, method) in outlier_detections:
+    outlier = method.fit_predict(X_t[0:, 1:], y_t)
+
+    previous_size = X_t.size
+    pd.DataFrame(outlier).to_csv('task1/results/outlier-decisions.csv', ',', index=False)
+
+    decisions[id] = outlier
+    print(name + ' detected ' + str(len([x for x in outlier if x < 0])) + ' outliers.')
+
+
+# decision function about outliers. true it inlier
+L = lambda i: (decisions['loc'][i] > 0)
+
+
+X_t = filter(lambda x: L(int(x[0])), X_t)
 pd.DataFrame(X_t).to_csv('task1/results/X_inliners.csv', ',', index=False)
 
-y_t = filter(lambda x: (outlier[int(x[0])] > 0), y_t.values)
+y_t = filter(lambda x: L(int(x[0])), y_t.values)
 pd.DataFrame(y_t).to_csv('task1/results/y_inliners.csv', ',', index=False)
+
+print(str(len([i for i in range(0, len(decisions['loc'])-1) if not L(i)])) + ' samples are considered outliers.')
 
 
 X_t = pd.read_csv('task1/results/X_inliners.csv', ',')
