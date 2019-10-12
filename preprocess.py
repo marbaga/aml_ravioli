14c6949f9ce8fa2c1696053ffe9b888f036030b1
Lorenzo Laneve <lore97drk@icloud.com>
2019-10-12 14:44:51 +0200

    Change parameters in Local Outlier Factor

--- preprocess.py
+++ preprocess.py
@@ -23,10 +23,12 @@ filler.fit(X_t, y_t)
 X_t = filler.transform(X_t)
 pd.DataFrame(X_t).to_csv('task1/results/filled.csv', ',', index=False)
 
+n = len([x for x in X_t]) - 1
 
+# changing n_neighbors to max(50, int(0.1*n)) considerably increases the number of outliers (default was 20)
 outlier_detections = [
-    ('if', 'Isolation Forest', IsolationForest(behaviour='new', contamination='auto')),
-    ('loc', 'Local Outlier Factor', LocalOutlierFactor(contamination='auto'))
+    #('if', 'Isolation Forest', IsolationForest(behaviour='new', contamination='auto')),
+    ('loc', 'Local Outlier Factor', LocalOutlierFactor(n_neighbors=max(50, int(0.1*n)), contamination='auto'))
 ]
 
 decisions = {}
@@ -39,10 +41,11 @@ for (id, name, method) in outlier_detections:
 
     decisions[id] = outlier
     print(name + ' detected ' + str(len([x for x in outlier if x < 0])) + ' outliers.')
+    print([i for i in range(0, len(outlier) - 1) if outlier[i] < 0])
 
 
-# decision function about outliers. true it inlier
-L = lambda i: (decisions['loc'][i] > 0)
+# decision function about outliers. true if inlier
+L = lambda sample_id: (decisions['loc'][sample_id] > 0)
 
 
 X_t = filter(lambda x: L(int(x[0])), X_t)
@@ -51,17 +54,18 @@ pd.DataFrame(X_t).to_csv('task1/results/X_inliners.csv', ',', index=False)
 y_t = filter(lambda x: L(int(x[0])), y_t.values)
 pd.DataFrame(y_t).to_csv('task1/results/y_inliners.csv', ',', index=False)
 
-print(str(len([i for i in range(0, len(decisions['loc'])-1) if not L(i)])) + ' samples are considered outliers.')
+outliers_count = len([i for i in range(0, len(decisions['loc'])-1) if not L(i)])
+print(str(outliers_count) + ' samples are considered outliers (~'+ str(int((outliers_count*100)/n)) +'% of the set).')
 
 
 X_t = pd.read_csv('task1/results/X_inliners.csv', ',')
 y_t = pd.read_csv('task1/results/y_inliners.csv', ',')
 
-X_t = X_t.drop('0', axis=1)
-
-scaler = StandardScaler()
-scaler.fit(X_t)
-
-X_t = scaler.transform(X_t)
-
-pd.DataFrame(X_t).to_csv('task1/results/X_normalized.csv', ',', index=False)
+#X_t = X_t.drop('0', axis=1)
+#
+#scaler = StandardScaler()
+#scaler.fit(X_t)
+#
+#X_t = scaler.transform(X_t)
+#
+#pd.DataFrame(X_t).to_csv('task1/results/X_normalized.csv', ',', index=False)
