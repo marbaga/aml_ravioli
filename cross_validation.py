5c8b898dc2b0b1b32a6097f4ec383da3b3bca7e1
Lorenzo Laneve <lore97drk@icloud.com>
2019-10-20 16:29:52 +0200

    Integrate correlation map feature selection into InputTransform estimator

--- cross_validation.py
+++ cross_validation.py
@@ -4,15 +4,15 @@ import numpy as np
 from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 
-from sklearn.linear_model import LinearRegression, Ridge, ElasticNet
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet, ElasticNetCV
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import cross_validate
 from estimator import InputTransform
 
 
 
-
-model = InputTransform(RandomForestRegressor(n_estimators=50), contamination=0.05)
+#model = InputTransform(ElasticNetCV(l1_ratio=0.5, eps=1e-3, n_alphas=10, cv=10, selection='random'), contamination=0.2)
+model = InputTransform(RandomForestRegressor(n_estimators=50), contamination=0.1)
 
 X_t = pd.read_csv('task1/X_train.csv', ',')
 y_t = pd.read_csv('task1/y_train.csv', ',')
--- estimator.py
+++ estimator.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import VarianceThreshold, SelectFromModel
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import LocalOutlierFactor
 
 import warnings
 
+from sklearn.preprocessing import StandardScaler
 
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
@@ -19,11 +20,18 @@ class InputTransform(BaseEstimator):
         self.model = model
         self.contamination = contamination
 
-    def fill_data(self, X, y):
-        filler = SimpleImputer(missing_values=np.nan, strategy='median')
-        filler.fit(X, y)
+        self.important_features = pd.read_csv('task1/best_features.csv', ',').to_numpy().flatten()
+        self.useless_features = pd.read_csv('task1/useless_features.csv', ',').to_numpy().flatten()
 
-        X = filler.transform(X)
+        self.included_columns = []
+        self.features_to_check = []
+
+
+    def fill_data(self, X, y):
+        self.filler = SimpleImputer(missing_values=np.nan, strategy='median')
+        self.filler.fit(X, y)
+
+        X = self.filler.transform(X)
 
         return (X, y)
 
@@ -34,14 +42,8 @@ class InputTransform(BaseEstimator):
         decisions = detector.fit_predict(X, y)
 
         print('Outlier detection detected ' + str(len([x for x in decisions if x < 0])) + ' outliers.')
-        #print([i for i in range(0, len(decisions) - 1) if decisions[i] < 0])
 
-        # decision function about outliers. true if inlier
         L = lambda sample_id: (decisions[sample_id] > 0)
-
-        #outliers_count = len([i for i in range(0, len(decisions) - 1) if not L(i)])
-        #print(str(outliers_count) + ' samples are considered outliers (~' + str(
-        #    int((outliers_count * 100) / n)) + '% of the set).')
 
         X = [X[i] for i in range(0, len(decisions) - 1) if decisions[i] > 0]
 
@@ -54,19 +56,48 @@ class InputTransform(BaseEstimator):
         X = pd.DataFrame(X)
         y = np.asarray(y).ravel()
 
-        self.var_threshold = VarianceThreshold(threshold=0.1)
-        X = self.var_threshold.fit_transform(X)
+        # Keep best features (re-merge later)
+        X_best = X.copy().filter(self.important_features)
 
-        clf = RandomForestClassifier(n_estimators=50)
-        clf = clf.fit(X, y)
+        self.features_to_check = []
+        for i in range(0, 831):
+            if i not in self.important_features and i not in self.useless_features:
+                self.features_to_check.append(i)
 
-        self.feature_selector = SelectFromModel(clf, prefit=True, max_features=200)
-        X = self.feature_selector.transform(X)
+        self.features_to_check = np.array(self.features_to_check)
+        print('Features to check: ' + str(self.features_to_check.size))
+
+        X = X.copy().filter(self.features_to_check)
+
+        # correlation with output
+        X.insert(X.shape[1], 'y', y, True)  # insert y
+
+        cor = pd.DataFrame(X).corr()
+        columns = np.full((cor.shape[0],), True, dtype=bool)
+        for i in range(cor.shape[0]):
+            for j in range(i + 1, cor.shape[0]):
+                if abs(cor.iloc[i, j]) > 0.85 or abs(cor.iloc[j, cor.shape[0] - 1]) < 0.03:
+                    if columns[j]:
+                        columns[j] = False
+
+        columns = columns[:-1].copy()
+
+        del X['y']
+
+        X = X.iloc[:, columns]
+        #X = X.iloc[:, :X.shape[1] - 1]  # remove y
+
+        self.included_columns = columns
+
+        X = pd.concat([X_best.reset_index(drop=True), X.reset_index(drop=True)], axis=1)
 
         print('Feature selection selected', X.shape[1], 'features.')
 
-        return (X, y)
+        self.scaler = StandardScaler()
+        self.scaler.fit(X)
+        X = pd.DataFrame(self.scaler.transform(X))
 
+        return (X, y)
 
     def fit(self, X, y, **fit_params):
         X, y = self.fill_data(X, y)
@@ -76,11 +107,20 @@ class InputTransform(BaseEstimator):
         return self.model.fit(X, y)
 
     def predict(self, X):
-        filler = SimpleImputer(missing_values=np.nan, strategy='median')
-        filler.fit(X)
+        # fill with training median
+        X = self.filler.transform(X)
 
-        X = filler.transform(X)
-        X = self.var_threshold.transform(X)
-        X = self.feature_selector.transform(X)
+        X = pd.DataFrame(X)
+        # copy best features
+        X_best = X.copy().filter(self.important_features)
+
+        # filter medium features
+        X = X.copy().filter(self.features_to_check)
+        X = X.iloc[:, self.included_columns]
+
+        # remerge best features
+        X = pd.concat([X_best.reset_index(drop=True), X.reset_index(drop=True)], axis=1)
+
+        X = self.scaler.transform(X)
 
         return self.model.predict(X)
